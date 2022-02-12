994db2c92da813be8a187f9e63eb6fd7d20da181
AlbertLepree <aflepree4765@eagle.fgcu.edu>
2022-02-11 23:11:53 -0500

    fixed some errors and changed 'PowerBI' scraping to 'AWS' scraping

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     nextPages.append(nextPage[-1])
 
     #get links to first i pages
-    for i in range(1):
+    for i in range(6):
         with urllib.request.urlopen(f'https://www.indeed.com{nextPage[-1]}') as f:
             source = f.read()
 
@@ -46,19 +46,27 @@ def main():
         theLinks = theLinks + getJobLinks(nextPages[j])
 
 
-    counter = [0,0,0,0,0] # initializes counter variable
+    counter = [0,0,0,0,0,0] # initializes counter variable
     # loops k times for k links and sums number of times target word was found in each link
     for k in range(len(theLinks)):
         # adds index of the array to its corresponding index: [1, 2, 3] + [1, 1, 1] = [2, 3, 4]
         for index, integer in enumerate(readLinks(theLinks[k])):
             counter[index] += integer
 
+    numLinks = len(theLinks)
+
     print(counter)
-    print(len(theLinks))
+    print(numLinks)
+
+
 
     # plots the data to a bar graph
-    df = pd.DataFrame({'lab': ['SQL', 'Power BI', 'Tableau', 'Python', 'Excel' ], 'val': [counter[0], counter[1], counter[2], counter[3], counter[4]]})
-    ax = df.plot.bar(x='lab', y='val', rot=0)
+    df = pd.DataFrame({'Qualification': ['SQL', 'AWS', 'Tableau', 'Python', 'Excel'], '%OfDataAnalystJobs': [(counter[0]/numLinks)*100,
+                                                                                                              (counter[1]/numLinks) * 100,
+                                                                                                              (counter[2]/numLinks) * 100,
+                                                                                                              (counter[3]/numLinks) * 100,
+                                                                                                              (counter[4]/numLinks) * 100]})
+    ax = df.plot.bar(x='Qualification', y='%OfDataAnalystJobs', rot=0, )
     plt.show()
 
 
@@ -81,19 +89,22 @@ def getJobLinks(url):
 
 # reads the links and counts how many times SQL is said in each one
 def readLinks(jobURL):
-    # print(jobURL)
+    #print(jobURL)
 
     counter=[]
 
     # split and clean
     list = requests.get(jobURL).text.split()
 
+
     # counts each skill from the job description
     sqlList = [w for w in list if re.findall('(.*)([Ss][Qq][Ll])(.*)', w)]
-    pbiList = [w for w in list if re.findall('(.*)([Pp]ower[ ]?[Bb][Ii])(.*)', w)]
+    awsList = [w for w in list if re.findall('^([Aa][Ww][Ss])$', w)]
     tbluList = [w for w in list if re.findall('(.*)([tT]ableau)(.*)', w)]
-    pthnList = [w for w in list if re.findall('(.*)([Pp]ython,)(.*)', w)] # Issue: sometimes increments/identifies when it is not on the page?
+    pthnList = [w for w in list if re.findall('(.*)([Pp]ython,)(.*)', w)]
     xlList = [w for w in list if re.findall('(.*)(Excel)(.*)', w)]
+    #pbiList = [w for w in list if re.findall('^([Pp]ower[Bb][Ii])$', w)]
+
 
     # if skill shows up increment corresponding array index by 1
     if sqlList:
@@ -101,7 +112,7 @@ def readLinks(jobURL):
     else:
         counter.append(0)
 
-    if pbiList:
+    if awsList:
         counter.append(1)
     else:
         counter.append(0)
@@ -121,7 +132,12 @@ def readLinks(jobURL):
     else:
         counter.append(0)
 
-    # print(counter)
+    # if pbiList:
+    #     counter.append(1)
+    # else:
+    #     counter.append(0)
+
+    #print(counter)
     return counter
 
 if __name__ == '__main__':
